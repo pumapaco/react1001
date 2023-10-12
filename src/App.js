375581579ce249cdc73ee03df5db442f1d41ac0f
santos <pumapacoperu@gmail.com>
2023-10-12 09:52:40 -0500

    v3 corrige nombre

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> SANTOS PUMA.
+          SANTOS <code>src/App.js</code> SANTOS PUMA P.
         </p>
         <a
           className="App-link"
